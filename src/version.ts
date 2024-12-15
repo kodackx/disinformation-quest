ef2f7affb120c5267e60de0a8cd29e8c15b88edf
Constantin Rusu <constantinrusu21@gmail.com>
2024-12-15 23:00:40 +0200

    added version file

---
+++ src/version.ts
@@ -0,0 +1,14 @@
+export const VERSION = {
+    current: '0.2.0',
+    releaseDate: '2023-12-XX',
+    changelog: {
+      '0.2.0': [
+        'Added full start to end flow',
+        // other changes
+      ],
+      '0.1.0': [
+        'Initial release with basic functionality',
+        // initial features
+      ]
+    }
+  };
